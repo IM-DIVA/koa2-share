01851ff80b784b0f9b47369cccd37b65701f43ab
im-diva <im-diva2foxmail.com>
2019-03-16 00:53:44 +0800

    nuxt基础以及示例代码

---
+++ DOCS/nuxt/pages/search.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>search
+    <ul>
+      <li v-for='(item, idx) in $store.state.city.list' :key='idx'>
+        <h1>{{item}}</h1>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  layout: 'search',
+  data() {
+    return {
+      list: []
+    }
+  },
+  async asyncData() {
+    let {
+      status,
+      data: { list }
+    } = await axios.get('http://localhost:3000/city/list')
+    if (status === 200) {
+      return {
+        list
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: aqua;
+}
+</style>
